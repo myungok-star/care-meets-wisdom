de42637f402f8e22e3c30868c9f30523539a55d7
myungok-star <myungok.star@gmail.com>
2017-01-22 22:49:00 -0800

    add seed data for senior and care group

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Senior.destroy_all
+CareGroup.destroy_all
+
+Senior.create([
+  {name: "Spencer Salazar", location: "San Francisco, CA", interests: "Tea time, Gardening"},
+  {name: "Kelly Alvarado", location: "New York, CA", interests: "Arts & Crafts, Books"},
+  {name: "Leroy Rhodes", location: "Houston, TX", interests: "Fishing, Movies"},
+  {name: "Kurt Gray", location: "Los Angeles, CA", interests: "Painting, Badminton" },
+  {name: "Donna Harrington", location: "Sausalito, CA", interests: "Yoga, Drawing"},
+  {name: "Erica Fitzgerald", location: "San Francisco, CA", interests: "Swimming, Movies"},
+  {name: "Veronica Lyons", location: "Detroit, MI", interests: "Books, Card Games"},
+  {name: "Chad Washington", location: "Los Angeles, CA", interests: "Arts & Crefts, Fashion, Yoga"}
+])
+
+CareGroup.create([
+  {name: "Starlight Home", location: "San Francisco", about:"Starlight Homecare is a wonderful place for the elderly, disabled, and pediatric patients that need home based care. Located in Teaneck, New Jersey, this establishment offers certified home health aid that caters to every individual need. From basic nursing needs to home based care, every employee in Starlight Homecare seeks to provide the best care possible for their clients.
+
+The office, located right off of Cedar Lane, is a majestic building that gives the impression of an older time, with the gentle creeping of branches cupping the corners of the building, you get the impression of honor and respect. When you walk through the doors, you are greeted by the receptionist, who then helps direct you where you need to go for aid. With Starlight, you are assured that no matter what services you need, you will receive the best possible care by certified nurses in both the field of elderly care and pediatric care.
+
+For those who want to be close to their family members, the worry for accurate care is no longer an issue. Located right in your own home, the certified team that is in charge of caring for your loved one will come to your home. And you don't have to worry about a stranger coming into your home, each employee is certified and backed by the agency, all for the purpose of providing the best care possible for all their patients.", contact:"866-242-5233"},
+  {name: "Victorian Manor", location: "San Francisco, CA", about:"Victorian Manor assisted living facility located in a quiet residential location. We at the Victorian Manor realize that selecting the right senior care facility is a very difficult choice that takes time and research. We would like to share an important fact with those considering our facility. We take great pride in offering seniors a safe haven to live and enjoy their retirement years. We also provide excellent care in a very nurturing environment. We provide care at a very affordable price to our residents. Residents are provided with private rooms or a semi-private room at Victorian Manor. In addition, we provide customized care for our seniors. We realize that some under our care require special attention. Perhaps, they require our care in washing, bathing, dressing, combing hair, or eating their meals. We gladly customize care to cater to their special needs.Additional services offered to seniors include medication management. We make sure that they take their medications and arrange for prescription refills. Victorian Manor assisted living facility also provides housekeeping and laundry services in a timely manner to make sure residents and the area is kept clean. We provide nutritious meals to residents that are prepared in our own kitchen. We are also able to provide specialize meals for senior residents at the request of the family. We believe in creating an environment where the elderly are allowed to enjoy an independent living arrangement. Thus, preserving their dignity and feelings of self worth. The warm and caring environment is a fantastic place for those with cognitive disabilities too. We provide special attention and care that will help them to thrive in the facility. One huge advantage is that your elderly parents are able to socialize with others in the family like atmosphere. Please contact us for more information.", contact:"866-242-5233"},
+  {name: "Deer Hill Care Home", location: "New York, CA", about:"Deer Hill Care Home is a six-bed facility, nestled along the Lafayette hillside.  The facility is wheel-chair accessible and both ambulatory and non-ambulatory individuals are welcome.  Each Resident is provided his or her own furnished room, for privacy and comfort.  DHCH has four large bathrooms, designed to meet the special needs of seniors.  There is also a lush backyard, nice for daytime strolls!", contact:"866-242-5233"},
+  {name: "Welcome Home Senior Residence", location: "Detroit, MI", about:"Welcome Home Senior Residence is a corporation owned facilities. Our goal is to provide upscale environment and professional service to the residents. Our staffs are experienced in all levels of Dementia, etc. We are licensed to provide care for dementia and Hospice residents", contact:"866-242-5233"},
+  {name: "Grace Homes", location: "Los Angeles, CA", about:"Grace Homes – “your home away from home”.
+”Feel and experience the warm caring atmosphere of our home”.
+We take pride in providing quality, safe and compassionate care.", contact:"866-242-5233"}
+])
