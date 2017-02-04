51f807b05750cf22e946eb8ffd99f59b7906cde2
myungok-star <myungok.star@gmail.com>
2017-02-04 10:19:54 -0800

    remove and update default avatar for each group

--- app/models/volunteer.rb
+++ app/models/volunteer.rb
@@ -1,5 +1,5 @@
 class Volunteer < ApplicationRecord
-    has_attached_file :avatar, styles: { medium: '200x200>', thumb: '100x100>' }, default_url: 'https://share.okdothis.com/assets/default-avatar-c246f74ff56d382424ac2c750e40b0a6.png?w=64&h=64'
+    has_attached_file :avatar, styles: { medium: '200x200>', thumb: '100x100>' }, default_url: 'http://i.imgur.com/9JlhmGx.png'
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 
     validates :name, presence: true
